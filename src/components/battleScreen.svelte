0ce0fbd6ec29ccefc3fd1d7cba7c2228101bd1ec
Ceribe <ceribekagami@gmail.com>
2022-03-02 22:56:29 +0100

    Add three.js

---
+++ src/components/battleScreen.svelte
@@ -0,0 +1,33 @@
+<script>
+	import * as THREE from 'three';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const scene = new THREE.Scene();
+
+		let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+		const renderer = new THREE.WebGLRenderer({
+			canvas: document.querySelector('#bg')
+		});
+
+		camera.position.setZ(30);
+
+		const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+		const torus = new THREE.Mesh(geometry, material);
+		scene.add(torus);
+		function render() {
+			requestAnimationFrame(render);
+			torus.rotation.y += 0.011;
+			renderer.render(scene, camera);
+			camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+			camera.position.setZ(30);
+			renderer.setPixelRatio(window.devicePixelRatio);
+			renderer.setSize((window.innerWidth / 4) * 3, (window.innerHeight / 4) * 3);
+		}
+		render();
+	});
+</script>
+
+<canvas id="bg" />
+sdfsd
