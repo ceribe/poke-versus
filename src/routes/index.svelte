aef1848da6a8e16f8bdf63547798def89be548cd
Ceribe <ceribekagami@gmail.com>
2022-03-02 09:23:18 +0100

    Add initial project

---
+++ src/routes/index.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { availablePokemon } from '../stores/dataStore';
+</script>
+
+<h1>Poké Versus</h1>
+
+<svelte:head>
+	<title>Poké Versus</title>
+</svelte:head>
+
+<div class="grid-container" id="grid">
+	{#each availablePokemon as pokemon}
+		<div class="grid-item">
+			<img
+				src="https://img.pokemondb.net/sprites/diamond-pearl/normal/{pokemon.toLowerCase()}.png"
+				alt={pokemon}
+			/>
+			{pokemon}
+		</div>
+	{/each}
+</div>
+
+<style>
+	.grid-container {
+		display: grid;
+		grid-template-columns: 190px 190px 190px 190px;
+		padding: 50px;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.grid-item {
+		background-color: rgba(255, 255, 255, 0.8);
+		border: 1px solid rgba(0, 0, 0, 0.8);
+		padding: 20px;
+		font-size: 30px;
+		text-align: center;
+	}
+</style>
---
+++ src/stores/dataStore.ts
@@ -0,0 +1,25 @@
+import { writable } from 'svelte/store';
+
+export const availablePokemon = [
+	'Umbreon',
+	'Glaceon',
+	'Gyarados',
+	'Vaporeon',
+	'Snorlax',
+	'Mewtwo',
+	'Garchomp',
+	'Lugia',
+	'Gengar',
+	'Dragonite',
+	'Blastoise',
+	'Groudon',
+	'Charizard',
+	'Tyranitar',
+	'Rayquaza',
+	'Gardevoir',
+	'Azumarill',
+	'Blaziken',
+	'Kyogre',
+	'Venusaur'
+];
+export const myPokemon = writable([]);
