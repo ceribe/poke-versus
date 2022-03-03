61fd0cda18e74b5fa546fe5b73c219f0b22da39a
Ceribe <ceribekagami@gmail.com>
2022-03-03 21:30:03 +0100

    Add type banners

--- src/components/typeBanner.svelte
+++ src/components/typeBanner.svelte
@@ -40,20 +40,66 @@
 	>
 		Ghost
 	</div>
+{:else if type == Type.Steel}
+	<div
+		class="bg-gray-400 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Steel
+	</div>
+{:else if type == Type.Normal}
+	<div
+		class="bg-stone-500 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Normal
+	</div>
+{:else if type == Type.Fighting}
+	<div
+		class="bg-red-800 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Fighting
+	</div>
+{:else if type == Type.Psychic}
+	<div
+		class="bg-pink-500 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Psychic
+	</div>
+{:else if type == Type.Dragon}
+	<div
+		class="bg-violet-700 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Dragon
+	</div>
+{:else if type == Type.Fairy}
+	<div
+		class="bg-pink-300 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Fairy
+	</div>
+{:else if type == Type.Dark}
+	<div
+		class="bg-stone-700 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Dark
+	</div>
+{:else if type == Type.Rock}
+	<div
+		class="bg-amber-500 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Rock
+	</div>
+{:else if type == Type.Electric}
+	<div
+		class="bg-yellow-300 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Electric
+	</div>
+{:else if type == Type.Ground}
+	<div
+		class="bg-amber-800 h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl border-4 border-black"
+	>
+		Ground
+	</div>
 {:else}
 	<div class=" h-10 flex justify-center items-center text-white font-semibold text-lg rounded-xl" />
 {/if}
-
-<!-- None,
-
-Steel,
-Normal,
-Fighting,
-Psychic,
-Dragon,
-Fairy,
-Dark,
-Rock,
-Electric,
-Ground 
--->
