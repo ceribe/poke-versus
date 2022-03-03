837c5b506a9344fdaacab8a847a9a55a1639a662
Ceribe <ceribekagami@gmail.com>
2022-03-03 20:21:11 +0100

    Add models

--- src/components/battleScreen.svelte
+++ src/components/battleScreen.svelte
@@ -1,33 +1,35 @@
 <script>
 	import * as THREE from 'three';
-	import { onMount } from 'svelte';
+	import * as SC from 'svelte-cubed';
+	import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+	import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+	import { availablePokemon } from '../stores/pokedex';
+	import { writable } from 'svelte/store';
 
-	onMount(async () => {
-		const scene = new THREE.Scene();
-
-		let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		const renderer = new THREE.WebGLRenderer({
-			canvas: document.querySelector('#bg')
+	const mtlLoader = new MTLLoader();
+	let models = writable(new Map());
+	let count = 0;
+	for (const pokemon of availablePokemon) {
+		mtlLoader.load(`models/${pokemon.name}/${pokemon.name}.mtl`, function (materials) {
+			materials.preload();
+			const loader = new OBJLoader();
+			loader.setMaterials(materials);
+			loader.load(`models/${pokemon.name}/${pokemon.name}.obj`, (object) => {
+				object.position.x = count;
+				count += 5;
+				$models.set(pokemon.name, object);
+				models.set($models);
+			});
 		});
-
-		camera.position.setZ(30);
-
-		const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-		const torus = new THREE.Mesh(geometry, material);
-		scene.add(torus);
-		function render() {
-			requestAnimationFrame(render);
-			torus.rotation.y += 0.011;
-			renderer.render(scene, camera);
-			camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-			camera.position.setZ(30);
-			renderer.setPixelRatio(window.devicePixelRatio);
-			renderer.setSize((window.innerWidth / 4) * 3, (window.innerHeight / 4) * 3);
-		}
-		render();
-	});
+	}
 </script>
 
-<canvas id="bg" />
-sdfsd
+<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
+	{#each [...$models] as [_, value]}
+		<SC.Primitive object={value} position={[0, 0, 0]} />
+	{/each}
+	<SC.AmbientLight intensity={1.9} />
+	<SC.DirectionalLight intensity={0.3} position={[-2, 3, 2]} />
+	<SC.PerspectiveCamera position={[0, 20, 40]} fov={75} near={0.1} far={1000} />
+	<SC.OrbitControls enableZoom={true} />
+</SC.Canvas>
