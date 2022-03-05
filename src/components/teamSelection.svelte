583282d87f28c49742c9961c41e160e7729c3101
Ceribe <ceribekagami@gmail.com>
2022-03-05 14:22:49 +0100

    Fix resource

--- src/components/teamSelection.svelte
+++ src/components/teamSelection.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="bg-blue-400 horiz flex content-center">
-	<img src="static/pokeball.png" alt="logo" class="h-12 w-11 ml-2 mt-2 mb-2" />
+	<img src="/pokeball.png" alt="logo" class="h-12 w-11 ml-2 mt-2 mb-2" />
 	<h1 class="text-2xl uppercase p-4 text-white font-semibold">Poké Versus</h1>
 </div>
 
