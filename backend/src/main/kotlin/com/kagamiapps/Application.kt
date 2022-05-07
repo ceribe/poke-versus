8546bde53c8997fd0aebbb2393d4c8bedfc8dede
Ceribe <ceribekagami@gmail.com>
2022-05-07 22:33:21 +0200

    Add some backend code

--- backend/src/main/kotlin/com/kagamiapps/Application.kt
+++ backend/src/main/kotlin/com/kagamiapps/Application.kt
@@ -3,9 +3,18 @@ package com.kagamiapps
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.kagamiapps.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.websocket.*
+import java.time.Duration
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = 8128, host = "0.0.0.0") {
+        install(WebSockets){
+            pingPeriod = Duration.ofSeconds(15)
+            timeout = Duration.ofSeconds(15)
+            maxFrameSize = Long.MAX_VALUE
+            masking = false
+        }
         configureRouting()
     }.start(wait = true)
 }
--- backend/src/main/kotlin/com/kagamiapps/plugins/Routing.kt
+++ backend/src/main/kotlin/com/kagamiapps/plugins/Routing.kt
@@ -1,19 +1,121 @@
 package com.kagamiapps.plugins
 
+import com.kagamiapps.buildByteArray
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.util.*
+import kotlin.collections.LinkedHashSet
 
-fun Application.configureRouting() {
+data class Player(val session: DefaultWebSocketSession, val pokemonIDs: MutableList<Int> = mutableListOf())
+data class Game(var player1: Player, var player2: Player, val id: Int)
 
-    // Starting point for a Ktor app:
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
+val games: MutableMap<Int, Game> = Collections.synchronizedMap(HashMap())
+val waitingPlayers: MutableSet<Player> = Collections.synchronizedSet(LinkedHashSet<Player>())
+
+fun getLowestAvailableGameId(): Int {
+    for (id in 0..7)
+        if (!games.containsKey(id))
+            return id
+    throw IllegalStateException("No more games available")
+}
+
+class BitArray(byteArray: ByteArray) {
+
+    private var value: Int = 0
+
+    init {
+        value = 0
+        for (i in byteArray.indices) {
+            value = value or (byteArray[i].toInt() shl 8 * i)
         }
     }
-    routing {
+
+    fun getInt(startIndex: Int, length: Int): Int {
+        var result = 0
+        for (i in startIndex until startIndex + length) {
+            result = result shl 1
+            if ((value shr i and 1) == 1)
+                result = result or 1
+        }
+        return result
     }
 }
+
+fun Application.configureRouting() {
+    routing {
+        webSocket("/pv") {
+            val newPlayer = Player(this)
+            for (frame in incoming) {
+                when (frame) {
+                    is Frame.Binary -> {
+                        val bits = BitArray(frame.readBytes())
+                        when (bits.getInt(0, 3)) {
+                            0 -> processJoinGameMessage(newPlayer, bits)
+                            2 -> processAttackMessage(newPlayer, bits)
+                            4 -> processReconnectMessage(newPlayer, bits)
+                        }
+                    }
+                    else -> {
+                        println("Unhandled frame: $frame")
+                    }
+                }
+            }
+        }
+    }
+}
+
+suspend fun processJoinGameMessage(player: Player, bits: BitArray) {
+    waitingPlayers += player
+    with (player.pokemonIDs) {
+        add(bits.getInt(3, 5))
+        add(bits.getInt(8, 5))
+        add(bits.getInt(13, 5))
+    }
+
+    if (waitingPlayers.size == 2) {
+        val newGameId = getLowestAvailableGameId()
+        val game = Game(waitingPlayers.first(), waitingPlayers.last(), newGameId)
+        waitingPlayers.clear()
+        games[newGameId] = game
+        sendOpponentJoinedMessages(game)
+
+    }
+}
+
+suspend fun sendOpponentJoinedMessages(game: Game) {
+    val message1 = buildByteArray {
+        add(1, 1)
+        add(game.id, 3)
+        add(game.player2.pokemonIDs[2], 5)
+        add(game.player2.pokemonIDs[1], 5)
+        add(game.player2.pokemonIDs[0], 5)
+        add(1,3)
+    }
+    game.player1.session.send(Frame.Binary(true, message1))
+
+    val message2 = buildByteArray {
+        add(0, 1)
+        add(game.id, 3)
+        add(game.player1.pokemonIDs[2], 5)
+        add(game.player1.pokemonIDs[1], 5)
+        add(game.player1.pokemonIDs[0], 5)
+        add(1,3)
+    }
+    game.player2.session.send(Frame.Binary(true, message2))
+}
+
+
+fun processAttackMessage(player: Player, bits: BitArray) {
+
+}
+
+fun sendReceiveDamageMessage() {
+
+}
+
+fun processReconnectMessage(player: Player, bits: BitArray) {
+
+}
+
